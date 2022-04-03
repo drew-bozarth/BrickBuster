183cf0900567de30c0afb8725c1ae5ecd090016f
Drew-bozarth <dbozarth@chapman.edu>
2022-04-02 21:00:43 -0700

    adding all files into new repository

---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levels : MonoBehaviour
+{
+    public Readouts Readouts;
+    public List<GameObject> levels;
+    private GameObject levelGameObject;
+    private int currentLevel = 0;
+
+    void Start()
+    {
+        levelGameObject = CreateLevel();
+        UpdateLevelReadouts();
+    }
+
+    public void GoToNextLevel()
+    {
+        currentLevel++;
+        if (IsGameOver())
+            return;
+        LoadNextLevel();
+    }
+
+    public bool IsGameOver()
+    {
+        if (currentLevel == levels.Count)
+            return true;
+        return false;
+    }
+
+    private void LoadNextLevel()
+    {
+        if (levelGameObject != null)
+            Destroy(levelGameObject);
+        levelGameObject = CreateLevel();
+        UpdateLevelReadouts();
+    }
+
+    private void UpdateLevelReadouts()
+    {
+        Readouts.ShowLevel(currentLevel + 1);
+    }
+
+    private GameObject CreateLevel()
+    {
+        return Instantiate(levels[currentLevel], levels[currentLevel].transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Readouts.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Readouts : MonoBehaviour
+{
+    public Text Score;
+    public Text BallsRemaining;
+    public Text Level;
+    public Text GameResult;
+
+    public void Reset(int startingNumberOfBalls)
+    {
+        ShowScore(0);
+        ShowLevel(1);
+        ShowBallsRemaining(startingNumberOfBalls);
+        HideWinResults();
+    }
+
+    public void ShowScore(int score)
+    {
+        if (score < 0)
+            score = 0;
+        Score.text = "Score: " + score;
+    }
+
+    public void ShowLevel(int level)
+    {
+        if (level < 0)
+            level = 0;
+        Level.text = "Level: " + level;
+    }
+
+    public void ShowBallsRemaining(int ballsRemaining)
+    {
+        if (ballsRemaining < 0)
+            ballsRemaining = 0;
+        BallsRemaining.text = "Balls Remaining: " + ballsRemaining;
+    }
+
+    public void ShowLoseResults()
+    {
+        GameResult.text = "LOSE";
+    }
+
+    public void ShowWinResults()
+    {
+        GameResult.text = "Win";
+    }
+
+    public void HideWinResults()
+    {
+        GameResult.text = "";
+    }
+}
